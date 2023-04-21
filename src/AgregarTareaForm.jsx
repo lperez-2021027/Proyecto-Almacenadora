60b85cb0ee133b4d1cbd8d41145e17f718205d69
Luis Pérez <lperez-2021027@kinal.edu.gt>
2023-04-21 01:02:11 -0600

    Tercer Commit

---
+++ src/AgregarTareaForm.jsx
@@ -0,0 +1,92 @@
+export const AgregarTareaForm = ( props ) => {
+
+    const borrarCamposFormulario = () => {
+        props.setNombreTarea("");
+        props.setDescripcionTarea("");
+        props.setFechaInicio("");
+        props.setFechaCierre("");
+        props.setCreadorTarea("");
+    };
+
+    const enviarFormulario = (event) => {
+        event.preventDefault();
+        const tarea = {
+            nombre: props.nombreTarea,
+            descripcion: props.descripcionTarea,
+            fechaInicio: props.fechaInicio,
+            fechaCierre: props.fechaCierre,
+            creador: props.creadorTarea,
+            estado: "en-progreso",
+        };
+        if (props.editandoTarea !== null) {
+            const nuevasTareas = [...props.tareas];
+            nuevasTareas[props.editandoTarea] = tarea;
+            props.setTareas(nuevasTareas);
+            props.setEditandoTarea(null);
+        } else {
+            props.setTareas([...props.tareas, tarea]);
+        }
+        event.target.reset();
+        borrarCamposFormulario()
+    };
+
+    return (
+        <>
+
+            {/* ------------------- FORMULARIO -------------------*/}
+            <form onSubmit={enviarFormulario} className="row row-cols-lg-auto g-3 justify-content-center align-items-center mb-4 pb-2">
+                <div className="col-12">
+                    <div className="form-outline">
+
+                        <input type="text" id="formNombreTarea"
+                            className="form-control" placeholder="Nombre de la tarea"
+                            value={props.nombreTarea} onChange={(e) => props.setNombreTarea(e.target.value)} />
+
+                    </div>
+                </div>
+                <div className="col-12">
+                    <div className="form-outline">
+
+                        <textarea id="formDescripcionTarea"
+                            className="form-control" placeholder="Descripción de la tarea"
+                            value={props.descripcionTarea} onChange={(e) => props.setDescripcionTarea(e.target.value)} />
+
+                    </div>
+                </div>
+                <div className="col-md-6">
+                    <div className="form-outline">
+
+                        <input type="date" id="formFechaInicio"
+                            className="form-control" value={props.fechaInicio} onChange={(e) => props.setFechaInicio(e.target.value)} />
+
+                    </div>
+                </div>
+                <div className="col-md-6">
+                    <div className="form-outline">
+
+                        <input type="date" id="formFechaCierre"
+                            className="form-control" value={props.fechaCierre} onChange={(e) => props.setFechaCierre(e.target.value)} />
+
+                    </div>
+                </div>
+                <div className="col-12">
+                    <div className="form-outline">
+
+                        <input type="text" id="formCreadorTarea"
+                            className="form-control" placeholder="Nombre de usuario"
+                            value={props.creadorTarea} onChange={(e) => props.setCreadorTarea(e.target.value)} />
+
+                    </div>
+                </div>
+
+                {/* ------------------- ENVIO FORMULARIO -------------------*/}
+                <div className="col-12">
+                    <button type="submit" className="btn btn-primary m-1">
+                        Guardar
+                    </button>
+                </div>
+            </form>
+        </>
+    )
+
+}
--- src/ListaTareas.jsx
+++ src/ListaTareas.jsx
@@ -1,6 +1,9 @@
 import { useEffect, useState } from "react";
+import { AgregarTareaForm } from "./AgregarTareaForm";
+import './assets/style.css'
 
 export const ListaTareas = () => {
+
     const [tareas, setTareas] = useState(JSON.parse(localStorage.getItem("tareas")) || []);
     const [editandoTarea, setEditandoTarea] = useState(null);
     const [nombreTarea, setNombreTarea] = useState("");
@@ -8,35 +11,10 @@ export const ListaTareas = () => {
     const [fechaInicio, setFechaInicio] = useState("");
     const [fechaCierre, setFechaCierre] = useState("");
     const [creadorTarea, setCreadorTarea] = useState("");
+    const [isChecked, setIsChecked] = useState(false);
 
-    const borrarCamposFormulario = () => {
-        setNombreTarea("");
-        setDescripcionTarea("");
-        setFechaInicio("");
-        setFechaCierre("");
-        setCreadorTarea("");
-    };
-
-    const enviarFormulario = (event) => {
-        event.preventDefault();
-        const tarea = {
-            nombre: nombreTarea,
-            descripcion: descripcionTarea,
-            fechaInicio: fechaInicio,
-            fechaCierre: fechaCierre,
-            creador: creadorTarea,
-            estado: "en-progreso",
-        };
-        if (editandoTarea !== null) {
-            const nuevasTareas = [...tareas];
-            nuevasTareas[editandoTarea] = tarea;
-            setTareas(nuevasTareas);
-            setEditandoTarea(null);
-        } else {
-            setTareas([...tareas, tarea]);
-        }
-        event.target.reset();
-        borrarCamposFormulario()
+    const handleCheckboxChange = (event) => {
+        setIsChecked(event.target.checked);
     };
 
     const eliminarTarea = (index) => {
@@ -46,7 +24,14 @@ export const ListaTareas = () => {
         }
         nuevasTareas.splice(index, 1);
         setTareas(nuevasTareas);
+        const a = document.getElementById("flexCheckDefault")
+        console.log(a.value);
+
     };
+
+    // let textSuccess = {
+    //     textDecoration: 'line-through'
+    // }
 
     useEffect(() => {
         const tareasGuardadas = localStorage.getItem("tareas");
@@ -61,86 +46,105 @@ export const ListaTareas = () => {
 
     return (
         <>
+            {/* ------------------- CONTAINER -------------------*/}
             <section className="vh-100">
                 <div className="container-fluid py-5 h-100">
                     <div className="row d-flex justify-content-center align-items-center h-100">
                         <div className="col col-lg-10 col-xl-10">
                             <div className="card rounded-3">
                                 <div className="card-body p-4 m-2">
-                                    <h4 className="text-center my-3 pb-3">To Do App</h4>
 
-                                    <form onSubmit={enviarFormulario} className="row row-cols-lg-auto g-3 justify-content-center align-items-center mb-4 pb-2">
-                                        <div className="col-12">
-                                            <div className="form-outline">
-                                                <input type="text" id="formNombreTarea" className="form-control" placeholder="Nombre de la tarea" value={nombreTarea} onChange={(e) => setNombreTarea(e.target.value)} />
-                                            </div>
-                                        </div>
-                                        <div className="col-12">
-                                            <div className="form-outline">
-                                                <textarea id="formDescripcionTarea" className="form-control" placeholder="Descripción de la tarea" value={descripcionTarea} onChange={(e) => setDescripcionTarea(e.target.value)} />
-                                            </div>
-                                        </div>
-                                        <div className="col-md-6">
-                                            <div className="form-outline">
-                                                <input type="date" id="formFechaInicio" className="form-control" value={fechaInicio} onChange={(e) => setFechaInicio(e.target.value)} />
-                                            </div>
-                                        </div>
-                                        <div className="col-md-6">
-                                            <div className="form-outline">
-                                                <input type="date" id="formFechaCierre" className="form-control" value={fechaCierre} onChange={(e) => setFechaCierre(e.target.value)} />
-                                            </div>
-                                        </div>
-                                        <div className="col-12">
-                                            <div className="form-outline">
-                                                <input type="text" id="formCreadorTarea" className="form-control" placeholder="Nombre y apellido del creador de la tarea" value={creadorTarea} onChange={(e) => setCreadorTarea(e.target.value)} />
-                                            </div>
-                                        </div>
-                                        <div className="col-12">
-                                            <button type="submit" className="btn btn-primary m-1">
-                                                Save
-                                            </button>
-                                        </div>
-                                    </form>
+                                    <h4 className="text-center my-3 pb-3">Lista de tareas</h4>
 
-                                    <table className="table mb-4">
-                                        <thead>
-                                            <tr>
-                                                <th scope="col">No.</th>
-                                                <th scope="col">tarea</th>
-                                                <th scope="col">descripcion</th>
-                                                <th scope="col">fechaInicio</th>
-                                                <th scope="col">fechaCierre</th>
-                                                <th scope="col">creador</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            {tareas.map((tarea, index) => (
-                                                <tr key={index}>
-                                                    <th scope="row">{index + 1}</th>
-                                                    <td>{tarea.nombre}</td>
-                                                    <td>{tarea.descripcion}</td>
-                                                    <td>{tarea.fechaInicio}</td>
-                                                    <td>{tarea.fechaCierre}</td>
-                                                    <td>{tarea.creador}</td>
-                                                    <td className="text-center">
-                                                        <button type="button" className="btn btn-danger m-1" onClick={() => eliminarTarea(index)}>
-                                                            Eliminar
-                                                        </button>
-                                                        <button type="button" className="btn btn-success m-1" onClick={() => {
-                                                            setNombreTarea(tarea.nombre);
-                                                            setDescripcionTarea(tarea.descripcion);
-                                                            setFechaInicio(tarea.fechaInicio);
-                                                            setFechaCierre(tarea.fechaCierre);
-                                                            setCreadorTarea(tarea.creador);
-                                                            setEditandoTarea(index);
-                                                        }}>
-                                                            Editar
-                                                        </button>
-                                                    </td>
+                                    <AgregarTareaForm
+                                        tareas={tareas}
+                                        setTareas={setTareas}
+                                        editandoTarea={editandoTarea}
+                                        setEditandoTarea={setEditandoTarea}
+                                        nombreTarea={nombreTarea}
+                                        setNombreTarea={setNombreTarea}
+                                        descripcionTarea={descripcionTarea}
+                                        setDescripcionTarea={setDescripcionTarea}
+                                        fechaInicio={fechaInicio}
+                                        setFechaInicio={setFechaInicio}
+                                        fechaCierre={fechaCierre}
+                                        setFechaCierre={setFechaCierre}
+                                        creadorTarea={creadorTarea}
+                                        setCreadorTarea={setCreadorTarea}
+                                    />
+
+                                    {/* ------------------- TABLA -------------------*/}
+                                    <div className="table-responsive">
+
+                                        <table className="table mb-4 table-hover">
+                                            <thead>
+                                                <tr>
+                                                    <th scope="col">No.</th>
+                                                    <th scope="col">Tarea</th>
+                                                    <th scope="col">Descripción</th>
+                                                    <th scope="col">Fecha de Inicio</th>
+                                                    <th scope="col">Fecha de Cierre</th>
+                                                    <th scope="col">Usuario</th>
+                                                    <th scope="col"></th>
                                                 </tr>
-                                            ))}
-                                        </tbody>
-                                    </table>
+                                            </thead>
+                                            <tbody>
+                                                {tareas.map((tarea, index) => (
+                                                    <tr key={index}>
+                                                        <th scope="row">{index + 1}</th>
+                                                        <td className={isChecked ? "completed" : ""} >
+                                                            {tarea.nombre}
+                                                        </td>
+                                                        <td className={isChecked ? "completed" : ""} >{
+                                                            tarea.descripcion}
+                                                        </td>
+                                                        <td className={isChecked ? "completed" : ""} >
+                                                            {tarea.fechaInicio}
+                                                        </td>
+                                                        <td className={isChecked ? "completed" : ""} >
+                                                            {tarea.fechaCierre}
+                                                        </td>
+                                                        <td className={isChecked ? "completed" : ""} >
+                                                            {tarea.creador}
+                                                        </td>
+                                                        {/* style={textSuccess} */}
+                                                        <td className="">
+                                                            <div className="text-center">
+
+                                                                <button type="button" className="btn btn-danger m-1" onClick={() => eliminarTarea(index)}>
+                                                                    Eliminar
+                                                                </button>
+
+                                                                <button type="button" className="btn btn-success m-1" onClick={() => {
+                                                                    setNombreTarea(tarea.nombre);
+                                                                    setDescripcionTarea(tarea.descripcion);
+                                                                    setFechaInicio(tarea.fechaInicio);
+                                                                    setFechaCierre(tarea.fechaCierre);
+                                                                    setCreadorTarea(tarea.creador);
+                                                                    setEditandoTarea(index);
+                                                                }}>
+                                                                    Editar
+                                                                </button>
+
+                                                            </div>
+                                                            <div className="form-check ">
+
+                                                                <input className="form-check-input" type="checkbox"
+                                                                    onChange={handleCheckboxChange} checked={isChecked}
+                                                                    id="flexCheckDefault" />
+
+                                                                <label className="form-check-label " >
+                                                                    Tarea realizada
+                                                                </label>
+                                                                
+                                                            </div>
+
+                                                        </td>
+                                                    </tr>
+                                                ))}
+                                            </tbody>
+                                        </table>
+                                    </div>
                                 </div>
                             </div>
                         </div>
